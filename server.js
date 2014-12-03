ce42ad84795d032b789b60caa0ff7d9ce0d79deb
scott-ellenoff <scott.ellenoff16@trinityschoolnyc.org>
2014-12-02 21:53:15 -0500

    adding /api
    just convention stuff that daniel brought up

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ var express= require('express');
 var app= express();
 var mongoose= require('mongoose');
 mongoose.connect('mongodb://localhost/test');
-app.post('/user', function(req, res){
+app.post('/api/user', function(req, res){
     
 });
-app.get('/:id/stats', function(req,res){
+app.get('/api/:id/stats', function(req,res){
     
 });
-app.get('/:id/games/active', function(req,res){
+app.get('/api/:id/games/active', function(req,res){
     
 });
-app.get('/:id/games/:gameid', function(req,res){
+app.get('/api/:id/games/:gameid', function(req,res){
     
 });
-app.get('/:id/games/all', function(req,res){
+app.get('/api/:id/games/all', function(req,res){
     
 });
-app.get('/:id/games/past', function(req,res){
+app.get('/api/:id/games/past', function(req,res){
     
 });
-app.post('/:id/games/:gameid', function(req, res){
+app.post('/api/:id/games/:gameid', function(req, res){
     
 });
 var server= app.listen(3000, function(){
