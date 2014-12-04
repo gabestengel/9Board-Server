f30fc051ea67c05cf3a9844768e0294546252202
scott-ellenoff <scott-ellenoff@users.noreply.github.com>
2014-12-04 11:20:45 -0500

    Basic return models?

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-var express= require('express');
-var app= express();
-var mongoose= require('mongoose');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
+var NineboardUser = require('./models/user.js');
+var NineboardGame = require('./models/game.js');
+var NineboardGameState = require('./models/game-state.js');
+var tree = app.createServer();
 app.post('/api/user', function(req, res){
     
 });
 app.get('/api/user/:id/stats', function(req,res){
-    
+    NineboardUser.find(function(err,game){
+        res.json(game);
+    });
 });
 app.get('/api/user/:id/games/active', function(req,res){
     
