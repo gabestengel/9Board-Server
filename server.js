fe7d784291b0bcc337df19630fe4012ab17cf3d4
scott-ellenoff <scott.ellenoff16@trinityschoolnyc.org>
2014-12-11 20:12:25 -0500

    full server overhaul
    I add almost all the functionality not sure if it works

--- server.js
+++ server.js
@@ -7,27 +7,110 @@ var NineboardGame = require('./models/game.js');
 var NineboardGameState = require('./models/game-state.js');
 var tree = app.createServer();
 app.post('/api/user', function(req, res){
-    
+    NineboardUser.save(function(err){
+        if(!err){
+            res.send("created");
+        }
+        else{
+            res.send(err);
+        }
+    });
 });
+//need some help understanding this function
 app.get('/api/user/:id/stats', function(req,res){
-    NineboardUser.find(function(err,game){
-        res.json(game);
+    NineboardUser.findById(req.params.id, function(err,game){
+        if(!err){
+            res.json(game);
+        }
+        else{
+            res.send(err);
+        }
     });
 });
 app.get('/api/user/:id/games/active', function(req,res){
-    
+    NineboardUser.findById(req.params.id,function(err){
+       if(!err){
+           NineboardGame.find(function(err, game){
+               if(!err){
+                   res.json(game);
+               }
+               else{
+                   res.send(err);
+               }
+           });
+       }
+       else{
+           res.send(err);
+       }
+    });
 });
 app.get('/api/user/:id/games/:gameid', function(req,res){
-    
+    NineboardUser.findById(req.params.id, function(err){
+        if(!err){
+            NineboardGame.findById(req.params.gameid, function(err, game){
+                if(!err){
+                    res.json(game);
+                }
+                else{
+                    res.send(err);
+                }
+            });
+        }
+        else{
+            res.send(err);
+        }
+    });
 });
 app.get('/api/user/:id/games/all', function(req,res){
-    
+    NineboardUser.findById(req.param.id, function(err){
+        if(!err){
+            NineboardGame.find(function(err, game){
+                if(!err){
+                    res.json(game);
+                }
+                else{
+                    res.send(err);
+                }
+            });
+        }
+        else{
+            res.send(err);
+        }
+    });
 });
 app.get('/api/user/:id/games/past', function(req,res){
-    
+    NineboardUser.findById(req.param.id, function(err){
+        if(!err){
+            NineboardGame.find(function(err, game){
+                if(!err){
+                    res.json(game);
+                }
+                else{
+                    res.send(err);
+                }
+            });
+        }
+        else{
+            res.send(err);
+        }
+    });
 });
 app.post('/api/:id/games/:gameid', function(req, res){
-    
+    NineboardUser.findById(req.param.id, function(err){
+        if(!err){
+            NineboardGame.find(req.param.gameid, function(err,game){
+               if(!err){
+                   res.json(game);
+                }
+                else{
+                    res.send(err);
+                }
+            });
+        }
+        else{
+            res.send(err);
+        }
+    });
 });
 var server= app.listen(3000, function(){
     var host= server.adress().adress;
