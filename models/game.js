edbae73ebe6eefc5b006c408b43e2bbaed9fe66f
scott-ellenoff <scott-ellenoff@users.noreply.github.com>
2015-01-08 14:14:41 -0500

    formatting, removed gameID

--- models/game.js
+++ models/game.js
@@ -3,19 +3,18 @@ var Schema       = mongoose.Schema;
 var gameStateSchema = require('../schemas/game-state.js');
 
 var gameSchema   = new Schema({
-    gameID: Number,
 	players: {
         	//Player1 is Xs, Player 2 is Os
         	player1Id: String,
         	player2Id: String
-    },
-    gameStatus: {
-        //Active, Paused, 12Quit, Done
-        ongoing: String,
-        ///Null, Player1, Player2
-        winner: String
-    },
-    gameStates: [gameStateSchema]
+    	},
+    	gameStatus: {
+		//Active, Paused, 12Quit, Done
+        	ongoing: String,
+        	///Null, Player1, Player2
+        	winner: String
+    	},
+    	gameStates: [gameStateSchema]
 });
 
 module.exports = mongoose.model('game', gameSchema);
