8fd244c2121a3912af09e7d8489e7c4afc0d2b28
maxHartman <max@5hearts.net>
2015-01-14 10:29:58 -0500

    new data

--- chartbuilder.js
+++ chartbuilder.js
@@ -1,6 +1,6 @@
 function highScoreList(element) {
     //var allScores = get from routes
-    var allScores = [{"username":"test1","score":10},{"username":"test2","score":8}];
+    var allScores = [{"username":"FacebookUser1","score":100},{"username":"FacebookUser2","score":50},{"username":"FacebookUser3","score":85}];
     allScores.sort(function(a,b) { return b.score - a.score});
     $(element).append('<table id = "scoreList"></table>');
         var newRow = $('<tr></tr>');
